786fe23a7b35e9b54e390ac67da0627f84679d4b
Juan Esteban Idarraga <esteban@servicetechnologies.biz>
2021-01-02 23:01:58 -0500

    finishing

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import '../assets/styles/components/Item.scss'
 import playIcon from '../assets/static/play-icon.png';
 import plusIcon from '../assets/static/plus-icon.png';
 
-const Item = () => (
+const Item = ( { cover, title, year, contentRating, duration } ) => (
     <div className="carousel-item">
-        <img className="carousel-item__img" src="https://picsum.photos/200/300" alt=""/>
+        <img className="carousel-item__img" src={cover} alt={title}/>
         <div className="carousel-item__details">
             <div>
                 <img className="carousel-item__details--img" src={playIcon} alt=""/>
                 <img className="carousel-item__details--img" src={plusIcon} alt=""/>
             </div>
-            <p className="carousel-item__details--title">title</p>
-            <p className="carousel-item__details--subtitle">description</p>
+            <p className="carousel-item__details--title">{title}</p>
+            <p className="carousel-item__details--subtitle">
+                {`${year} ${contentRating} ${duration}`} 
+            </p>
 
         </div>
     </div>
 );
+
+Item.propTypes = {
+    cover: PropTypes.string,
+    title: PropTypes.string,
+    year: PropTypes.number,
+    contentRating: PropTypes.string,
+    duration: PropTypes.number
+}
 
 export default Item;
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,28 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import Item from '../components/Item';
 import Footer from '../components/Footer';
+import useInitialState from '../hooks/useInitialState'
 
 import '../assets/styles/App.scss';
 
+const API = 'http://localhost:3000/initalState/';
 
-const App = () => (
-    <div className="App">
-        <Header />
-        <Search />
+const App = () => {
+    const initialState = useInitialState(API);
+    return (
+        <div className="App">
+            <Header />
+            <Search />
+            {
+                initialState.mylist.length > 0 &&
+                <Categories title="My list">
+                    <Carousel>
+                        <Item />
+                    </Carousel>
+                </Categories>
+            }
 
-        <Categories title="My list">
-            <Carousel>
-                <Item></Item>
-                <Item></Item>
-            </Carousel>
-        </Categories>
+            <Categories title="Trends">
+                <Carousel>
+                    {initialState.trends.map(item =>
+                        <Item key={item.id} {...item} />       
+                    )}
+                </Carousel>
+            </Categories>
 
-        <Footer />
-    </div>
-);
+            <Categories title="Originals">
+                <Carousel>
+                    {initialState.originals.map(item =>
+                        <Item key={item.id} {...item} />       
+                    )}
+                </Carousel>
+            </Categories>
+
+            <Footer />
+        </div>
+    );
+}
 
 export default App;
